28e1aa2e1052cf4c6d02b5f371a8f97fa857fe47
Prathamesh Anwekar <75598624+anwprath@users.noreply.github.com>
2026-09-23 18:58:40 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut mp = HashMap::new();
+
+        for num in nums {
+            let count = mp.entry(num).or_insert(0);
+            *count += 1;
+        }
+
+        let mut heap = BinaryHeap::new();
+
+        for (num, freq) in mp {
+            heap.push(Reverse((freq, num)));
+            if heap.len() > k as usize {
+                heap.pop();
+            }
+        }
+
+        heap.into_iter().map(|Reverse((_, num))| num).collect()
+    }
+}
